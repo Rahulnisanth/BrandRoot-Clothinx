883f884cc0e4dcb09f710fde1ed4f52084529ab5
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-03-24 16:09:00 +0530

    final-frontEndCommit

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -15,7 +15,7 @@ const ProductCard = ({ product }) => {
       <img src={imageUrl} />
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price"> {price}</span>
+        <span className="price">${price}</span>
       </div>
       <Button buttonType="inverted" onClick={addProductToCart}>
         Add to cart
